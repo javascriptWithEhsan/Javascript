e9b04f2fd1850e70a7a400f2c7ea9dbdc3a04738
javascriptWithEhsan <163157136+javascriptWithEhsan@users.noreply.github.com>
2024-03-26 22:54:09 +0000

    change

---
+++ 03Basics/scope.js
@@ -0,0 +1,61 @@
+//var c = 300
+let a = 300
+if (true) {
+    let a = 10
+    const b = 20
+    // console.log("INNER: ", a);
+    
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+function one(){
+    const username = "hitesh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+     two()
+
+}
+
+// one()
+
+if (true) {
+    const username = "hitesh"
+    if (username === "hitesh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num){
+    return num + 1
+}
+// if you directly declare functions you can write function addone before the function declaration 
+// but in case of addtwo you can not write addtwo before function declaration bcz this function is stored 
+// in the variable first this concept is known as hoisting
+
+
+
+addTwo(5)
+const addTwo = function(num){
+    return num + 2
+}
